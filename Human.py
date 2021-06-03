af9c339c320d9cf696bdac076816590a825b5714
zahzasm <zahzasm@gmail.com>
2021-06-03 16:19:43 +0200

    Adding Human Class

---
+++ Human.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python
+# title           :Human.py
+# description     :Enter Description Here
+# author          :Sebastian Maldonado
+# date            :6/3/21
+# version         :0.0
+# usage           :SEE README.md
+# notes           :Enter Notes Here
+# python_version  :3.6.8
+# conda_version   :4.8.3
+# =================================================================================================================
+
+
+class Human:
+	def __init__(self, name, gender):
+		self.name = name
+		self.gender = gender
